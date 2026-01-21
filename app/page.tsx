b08272a1e3036ff4706f4170d93a5964ccf632c6
kishi1997 <tomo9634@gmail.com>
2026-01-20 16:49:39 -0800

    IELTS添削機能のコア実装 (UI刷新とfetch化)

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,68 @@
-import Image from 'next/image';
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Field, FieldLabel, FieldDescription } from '@/components/ui/field';
+import { Textarea } from '@/components/ui/textarea';
+import { useState } from 'react';
 
 export default function Home() {
+  const [answer, setAnswer] = useState('');
+  const [aiResponse, setAiResponse] = useState('');
+
+  const sendAnswer = async (text: string) => {
+    const res = await fetch('/api/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        text,
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+    setAiResponse(data.output);
+  };
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{' '}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{' '}
-            or the{' '}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{' '}
-            center.
+    <div className="flex min-h-screen items-center justify-center bg-black font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl items-center justify-between px-16 py-32 sm:items-start dark:bg-black">
+        <div className="flex w-[60%] flex-col gap-4">
+          <div>
+            <img src="./test.png" alt="test=image" width={300} height={600} />
+          </div>
+          <p>
+            Describe the scene shown in the image below. You should write at
+            least 150 words. Include details about the people, the place, and
+            what is happening.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="bg-foreground text-background flex h-12 w-full items-center justify-center gap-2 rounded-full px-5 transition-colors hover:bg-[#383838] md:w-[158px] dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+        <div className="w-[40%]">
+          <Field>
+            <FieldLabel htmlFor="textarea-message" className="text-white">
+              Answer
+            </FieldLabel>
+            <FieldDescription>Enter your answer below.</FieldDescription>
+            <Textarea
+              id="textarea-message"
+              placeholder="Type your answer here."
+              name="message"
+              rows={4}
+              className="w-full border border-zinc-300 p-2 text-white dark:border-zinc-700"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          </Field>
+          <Button
+            variant="outline"
+            type="submit"
+            className="mt-2 hover:cursor-pointer"
+            onClick={() => sendAnswer(answer)}
           >
-            Documentation
-          </a>
+            Send
+          </Button>
+          <div className="mt-4 text-white">
+            {aiResponse ? aiResponse : 'AIのreviewが表示されます'}
+          </div>
         </div>
       </main>
     </div>
